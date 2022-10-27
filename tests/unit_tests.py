7111ef3b5aa1492ff61a94a72818137d93f61e66
AnnaHirschmann <anna.hirschmann@colorado.edu>
2022-10-27 17:37:49 +0000

    Failing test for writing matrix to file.

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,18 +2,21 @@ import unittest
 import random
 import numpy as np
 import sys
+import os
 sys.path.append('../')
 import data_processor  # nopep8
 
 class TestUtils(unittest.TestCase):
-    #@classmethod
-    #def setUpClass(cls):
-        
+    @classmethod
+    def setUpClass(cls):
+        cls.test_file_name = 'test_file.txt'
+        f = open(cls.test_file_name, 'w')
+        f.close()
 
-    #@classmethod
-    #def tearDownClass(cls):
+    @classmethod
+    def tearDownClass(cls):
         #  clears the random class initialized variables
-        #cls.
+        os.remove(cls.test_file_name)
 
     def test_get_random_matrix(self):
         num_rows = random.randint(1, 10)
@@ -25,8 +28,19 @@ class TestUtils(unittest.TestCase):
         self.assertIsInstance(matrix[0, 0], float)
 
     def test_get_file_dimensions(self):
-        iris_dim = data_processor.get_file_dimensions('iris.data')
+        iris_dim = data_processor.get_file_dimensions('../iris.data')
         self.assertEqual(iris_dim, (150, 5))
+        
+    def test_write_matrix_to_file(self):
+        num_rows = random.randint(1, 10)
+        num_columns = random.randint(1, 10)
+        #f = open(self.test_file_name)
+        
+        matrix = data_processor.write_matrix_to_file(num_rows, num_columns,
+                                                     self.test_file_name)
+        
+        self.assertEqual(matrix.shape, (num_rows, num_columns))
+        f.close()
         
         
 if __name__ == "__main__":
